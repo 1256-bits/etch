b36097669e18617bf240fab1049a09801b9c6c1d
128bit <128bit@localhost>
2022-11-20 16:18:32 +0300

    Add mouseover color trail

--- etch.js
+++ etch.js
@@ -1,7 +1,13 @@
 const container = document.querySelector("#container");
 const div = document.createElement("div");
+div.classList.toggle("pixel");
 
-console.log(container)
 for (let i = 0; i < 16; i++) {
     container.appendChild(div.cloneNode(false));
 }
+
+const pixels = document.querySelectorAll(".pixel");
+pixels.forEach(pxl => {
+	pxl.addEventListener("mouseover", () => pxl.style.backgroundColor = "black");
+	pxl.addEventListener("mouseout", () => pxl.style.backgroundColor = "white");
+});
