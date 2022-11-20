f04acc5ff085a00fa6d4244a151cddb890143bfe
128bit <128bit@localhost>
2022-11-20 17:41:22 +0300

    Make the grid resizable

--- etch.js
+++ etch.js
@@ -1,13 +1,19 @@
 const container = document.querySelector("#container");
 const div = document.createElement("div");
 div.classList.toggle("pixel");
+const root = document.querySelector(":root");
 
-for (let i = 0; i < 16; i++) {
-    container.appendChild(div.cloneNode(false));
+function resizeGrid(side) {
+	for (let i = 0; i < Math.pow(side,2); i++) {
+		container.appendChild(div.cloneNode(false));
+	}
+	root.style.setProperty("--side", side);
+
+	const pixels = document.querySelectorAll(".pixel");
+	pixels.forEach(pxl => {
+		pxl.addEventListener("mouseover", () => pxl.style.backgroundColor = "black");
+		pxl.addEventListener("mouseout", () => pxl.style.backgroundColor = "white");
+	});
 }
 
-const pixels = document.querySelectorAll(".pixel");
-pixels.forEach(pxl => {
-	pxl.addEventListener("mouseover", () => pxl.style.backgroundColor = "black");
-	pxl.addEventListener("mouseout", () => pxl.style.backgroundColor = "white");
-});
+resizeGrid(4);
